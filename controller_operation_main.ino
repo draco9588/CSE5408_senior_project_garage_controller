e83041db1db3ae67bf7e703909066c90e8a48860
Ricardo Torres <72730333+draco9588@users.noreply.github.com>
2021-04-09 10:14:29 -0700

    Add files via upload

---
+++ controller_operation_main.ino
@@ -0,0 +1,26 @@
+
+/*
+CSE 5408 Senior Project
+Garage Door Controller
+Created by Ricardo Torres, Cameron Wood, Pablo Fernando
+This Application is to be used with:
+Ardiuno Uno, ESP-01, Reed switch, and Garage Door Opener.
+The function of this file is to have the Ardiuno Uno 
+controller the functionalty of the Garage Door opener.
+With the Aid of ESP01 the controller will interact with 
+a home network to beable to use a phone to open or close the garage.
+*/
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+}
