6ac3172df6e2c68b9702acfca41fc7d4d025ec85
Ricardo Torres <72730333+draco9588@users.noreply.github.com>
2021-04-20 13:35:17 -0700

    update flutter

--- flutter_app_garage_controller/lib/main.dart
+++ flutter_app_garage_controller/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() =>  runApp(ControllerApp());
@@ -8,65 +10,29 @@ class ControllerApp extends StatelessWidget
   Widget build(BuildContext context)
   {
     return MaterialApp(
-      title: 'CSE Garage Controller',
-      theme: ThemeData(
-        primaryColor: Colors.white,
+      home: Scaffold(
+       appBar: AppBar(
+          title: Text('CSE Garage Controller'),
+         centerTitle: true,
       ),
-      home: RandomWords(),
-    );
+        body: Center(
+          child: RaisedButton(
+            onPressed: ()
+            {
+              print('Click');
+            },
+            child: Text('A button '),
+          ),
+      ),
+
+      ),
+      );
+
+
   }
 }
 
 
 
-
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = TextStyle(fontSize: 18.0);
-  @override
-
-  Widget build(BuildContext context)
-  {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions()
-  {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i)
-        {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/2;
-          if (index >= _suggestions.length)
-          {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair)
-  {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
-  }
-}
 
 
